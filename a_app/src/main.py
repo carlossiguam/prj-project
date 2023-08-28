57828c0d7e49c1637bd7b5761dbfc1aebf9e71c2
carlos sigua <carlos.sigua@gmail.com>
2023-08-28 12:39:52 -0500

    corrrigiendo mensajes

--- a_app/src/main.py
+++ a_app/src/main.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def terminal():
-    return 'Hola estamos iniciando con flask desde el contenedor o cuyes!!!!!'
+    return 'Hola estamos iniciando con flask desde el contenedor o guacamayas!!!!!'
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
